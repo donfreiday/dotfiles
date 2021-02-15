c87dd87a10e77debf894c294306b880fe77effda
Don Freiday <donfreiday@gmail.com>
2021-02-15 10:21:44 -0500

    .screenlayout: Clean up home layout and fix positioning

--- .screenlayout/home-desk-right-vertical.sh
+++ .screenlayout/home-desk-right-vertical.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --mode 1920x1080 --pos 1920x0 --rotate right --output DP2 --off --output DP3 --mode 1920x1080 --pos 0x474 --rotate normal --output VIRTUAL1 --off
 xrandr --newmode "1920x1080_74.00"  217.75  1920 2064 2264 2608  1080 1083 1088 1129 -hsync +vsync
 xrandr --addmode DP3 1920x1080_74.00
 xrandr --addmode DP1 1920x1080_74.00
-xrandr --output DP3 --mode 1920x1080_74.00
-xrandr --output DP1 --mode 1920x1080_74.00
-
+xrandr --output eDP1 --off --output DP1 --mode 1920x1080_74.00 --pos 1920x0 --rotate right --output DP2 --off --output DP3 --mode 1920x1080_74.00 --pos 0x567 --rotate normal --output VIRTUAL1 --off
