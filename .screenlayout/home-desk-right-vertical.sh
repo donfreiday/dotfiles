e27843a982e103dfde165920fc1cc21a7e20d146
Don Freiday <donfreiday@gmail.com>
2021-03-04 07:19:52 -0500

    .screenlayout: Overclock refresh rate in home monitor profile

--- .screenlayout/home-desk-right-vertical.sh
+++ .screenlayout/home-desk-right-vertical.sh
@@ -2,4 +2,4 @@
 xrandr --newmode "1920x1080_74.00"  217.75  1920 2064 2264 2608  1080 1083 1088 1129 -hsync +vsync
 xrandr --addmode HDMI2 1920x1080_74.00
 xrandr --addmode HDMI3 1920x1080_74.00
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate right --output HDMI3 --mode 1920x1080 --pos 0x569 --rotate normal --output VIRTUAL1 --off --output VGA-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080_74.00 --pos 1920x0 --rotate right --output HDMI3 --mode 1920x1080_74.00 --pos 0x569 --rotate normal --output VIRTUAL1 --off --output VGA-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
